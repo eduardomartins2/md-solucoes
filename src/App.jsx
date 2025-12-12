c0fda6412ad1037ef2303bcbe61546169ee1aa01
eduardo <eduardomartinsdev2@gmail.com>
2025-12-12 15:19:31 -0300

    chore: initial clean project structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Services />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
