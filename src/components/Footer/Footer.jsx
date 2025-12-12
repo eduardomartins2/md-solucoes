60ef400191c1f5ab3ecf628e6b6fa50284b57fa9
eduardo <eduardomartinsdev2@gmail.com>
2025-12-12 16:20:03 -0300

    feat: add Footer component with branding and updated color bg Header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
+import "./Footer.css";
+
 export default function Footer() {
   return (
-    <footer>
-      Footer
+    <footer className="footer">
+      <div className="footer-container">
+        <h3 className="footer-logo">MD Soluções</h3>
+
+        <p className="footer-text">
+          Suporte técnico, desenvolvimento web e consultoria especializada.
+        </p>
+
+        <p className="footer-copy">
+          © {new Date().getFullYear()} MD Soluções — Todos os direitos reservados.
+        </p>
+      </div>
     </footer>
   );
 }
