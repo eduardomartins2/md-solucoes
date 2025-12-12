a158cb1ef7bb3af9d776d11179f3a5fdb7e493b8
eduardo <eduardomartinsdev2@gmail.com>
2025-12-12 16:03:29 -0300

    feat: add Hero component and update styles to new color palette

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,19 @@
+import "./Hero.css";
+
 export default function Hero() {
   return (
-    <section>
-      Hero
+    <section className="hero">
+      <div className="hero-content">
+        <h1>MD Soluções em Tecnologia</h1>
+        <p>
+          Desenvolvimento de sites, manutenção em sistemas, suporte técnico e
+          soluções digitais sob medida para o seu negócio.
+        </p>
+
+        <a href="#contato" className="hero-btn">
+          Fale Conosco
+        </a>
+      </div>
     </section>
   );
 }
