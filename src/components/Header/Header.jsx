175a4075ba318d5ea7de3af02de2d4ed5d42cff1
eduardo <eduardomartinsdev2@gmail.com>
2025-12-12 15:32:33 -0300

    feat: cria componente Header funcional com estilo inicial

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,17 @@
+import "./Header.css";
+
 export default function Header() {
   return (
-    <header>
-      Header
+    <header className="header">
+      <div className="header-container">
+        <h1 className="logo">MD Soluções</h1>
+
+        <nav className="nav">
+          <a href="#">Início</a>
+          <a href="#">Serviços</a>
+          <a href="#">Contato</a>
+        </nav>
+      </div>
     </header>
   );
 }
